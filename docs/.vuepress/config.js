85e1a44f312f5a9ac11c8fc988bbb62796f6295c
u <u@u.com>
2021-01-21 17:01:53 +0800

    no message

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,69 @@
+const {description} = require('../../package')
+
+module.exports = {
+
+    title: 'Peter的个人博客',
+    description: description,
+    base: '/blog',
+    head: [
+        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}]
+    ],
+    themeConfig: {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        nav: [
+            {
+                text: 'Java',
+                link: '/java/',
+            },
+            {
+                text: 'Framework',
+                link: '/config/'
+            },
+            {
+                text: 'UI',
+                link: '/ui/'
+            },
+            {
+                text: '性能优化',
+                link: '/optimize/'
+            },
+            {
+                text: 'NDK',
+                link: '/ndk/'
+            },
+            {
+                text: '开源框架',
+                link: '/open-framework/'
+            },
+            {
+                text: 'jetpack',
+                link: '/jetpack/'
+            },
+            {
+                text: '关于我',
+                link: 'https://v1.vuepress.vuejs.org'
+            }
+        ],
+        sidebar: {
+            '/java/': [
+                {
+                    collapsable: false,
+                    children: [
+                        '',
+                        'using-vue',
+                    ]
+                }
+            ],
+        }
+    },
+    plugins: [
+        '@vuepress/plugin-back-to-top',
+        '@vuepress/plugin-medium-zoom',
+    ]
+}
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: 向阳而生
+actionText: 开启大门 →
+actionLink: /java/
+features:
+- title: 生活点滴
+  details: 人生苦短，没必要和生活过于计较
+- title: 开发点滴
+  details: 多总结，多思考，少写代码
+- title: 面试杂记
+  details: 记录一些面试题
+footer: Made by peter with ❤️
+---
