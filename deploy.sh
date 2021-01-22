82f1aab3a50e3f5a6bdfa2251e6da81a76f8655e
u <u@u.com>
2021-01-22 13:48:14 +0800

    提交的md记录

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,16 @@ yes | cp -r ./* ../../../../blog_build/blog
 
 cd ../../../../blog_build/blog
 
+#/Users/u/Desktop/my_blog/blog_build/blog
+#/Users/u/Desktop/my_blog/blog
+
 git add .
-git commit -m "初始化1"
+git commit -m "提交的产物"
+git push -f
+
+cd ../../blog
+git add .
+git commit -m "提交的md记录"
 git push -f
 
 cd -
